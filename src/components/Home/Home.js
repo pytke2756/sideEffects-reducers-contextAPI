58bb2bac81fd0cc41b6104430746e50be665f29c
Levente Ibrányi <levente.ibranyi@dpdgroup.com>
2022-10-07 14:34:46 +0200

    Custom context provider component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from "react";
+import AuthContext from "../../context/auth-context";
 
-import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
+import Card from "../UI/Card/Card";
+import classes from "./Home.module.css";
+import Button from "../UI/Button/Button"
 
 const Home = (props) => {
+  const autchCtx = useContext(AuthContext);
+
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={autchCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
